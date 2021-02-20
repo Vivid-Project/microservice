bfef27efd3a51c1e1372bc97275f35ddd38115c4
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-02-20 13:58:07 -0700

    Add return app and fix import paths

--- api/services/app.py
+++ api/services/app.py
@@ -1,28 +1,18 @@
 from flask import Flask, jsonify, request
-from tone_analyzer_service import ToneAnalyzerService
+from api.services.tone_analyzer_service import ToneAnalyzerService
 
-app = Flask(__name__)
-
-
-# create new tone from dream journal entry text
-@app.route('/microservice/api/v1.0/tones', methods=['POST'])
-def get_tones():
-
-    dream_text = str(request.data)
-
-    tone_analysis = ToneAnalyzerService.get_tones(dream_text)
-
-    return jsonify({'tone_analysis': tone_analysis})
+def microservice():
+    app = Flask(__name__)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # create new tone from dream journal entry text
+    @app.route('/microservice/api/v1.0/tones', methods=['POST'])
+    def get_tones():
 
+        dream_text = str(request.data)
 
-# get tone by id ? Might not be needed...
+        tone_analysis = ToneAnalyzerService.get_tones(dream_text)
 
-# @app.route('/microservice/api/v1.0/tones/<tone_id>', methods=['GET'])
-# def find_single_tone(tone_id):
-#     if tone_id not in tones:
-#         return '', 404
-#     return jsonify(tones[tone_id])
+        return jsonify({'tone_analysis': tone_analysis})
+
+    return app
