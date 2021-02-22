eb2ae2d29c48cf5bb73baceafc5e904f2c0febed
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-02-22 08:09:54 -0700

    Create a method to find tone names

---
+++ api/resources/format.py
@@ -0,0 +1,20 @@
+import json
+from collections import Counter
+
+class Format:
+    def format_tone_results(data):
+
+        new = data["tone_analysis"]["sentences_tone"]
+
+        def find_tone_names(data):
+            counter = 0
+            tone_names = []
+            while counter != len(data):
+                if not data[counter]["tones"]:
+                    counter += 1
+                else:
+                    tone_names.append(data[counter]["tones"][0]["tone_name"])
+                    counter += 1
+            return tone_names
+
+        
