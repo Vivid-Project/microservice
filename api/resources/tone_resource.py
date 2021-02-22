a7d96fde4c93cd4fda9aba706ae4cb210f525e4d
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-02-22 08:08:32 -0700

    Delete tone resource

--- api/resources/tone_resource.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource, reqparse
-
-class ToneResource(Resource):
-    def get(self):
-         parser = reqparse.RequestParser()
-
-         import code; code.interact(local=dict(globals(), **locals()))
