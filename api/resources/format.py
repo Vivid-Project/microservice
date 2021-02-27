bfef768f06556c5f9e04d71f542119f9f65a4670
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-02-27 16:09:39 -0700

    Add if statement to catch no punctuation edge case

--- api/resources/format.py
+++ api/resources/format.py
@@ -3,31 +3,34 @@ from collections import Counter
 class Format:
     def format_tone_results(data):
 
-        new = data["tone_analysis"]["sentences_tone"]
+        if len(data["tone_analysis"]) == 1:
+            return "Error: Please add punctuation to your dream journal entry"
+        else:
+            new = data["tone_analysis"]["sentences_tone"]
 
-        def find_tone_names(data):
-            counter = 0
-            tone_names = []
-            while counter != len(data):
-                if not data[counter]["tones"]:
-                    counter += 1 # pragma: no cover
-                else:  
-                    tone_names.append(data[counter]["tones"][0]["tone_name"])
-                    counter += 1
-            return tone_names
+            def find_tone_names(data):
+                counter = 0
+                tone_names = []
+                while counter != len(data):
+                    if not data[counter]["tones"]:
+                        counter += 1 # pragma: no cover
+                    else:
+                        tone_names.append(data[counter]["tones"][0]["tone_name"])
+                        counter += 1
+                return tone_names
 
-        def find_unique_tone_names(data):
-            unique_list = []
-            for x in data:
-                if x not in unique_list:
-                    unique_list.append(f'{x}')
-            return ", ".join(unique_list)
+            def find_unique_tone_names(data):
+                unique_list = []
+                for x in data:
+                    if x not in unique_list:
+                        unique_list.append(f'{x}')
+                return ", ".join(unique_list)
 
-        all_tones = find_tone_names(new)
-        unique_tones = find_unique_tone_names(all_tones)
-        strength_of_tone = Counter(all_tones)
+            all_tones = find_tone_names(new)
+            unique_tones = find_unique_tone_names(all_tones)
+            strength_of_tone = Counter(all_tones)
 
-        return {
-            "unique_tones": unique_tones,
-            "tone_strength": strength_of_tone
+            return {
+                "unique_tones": unique_tones,
+                "tone_strength": strength_of_tone
         }
