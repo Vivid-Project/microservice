4c577944ad32bfdb36a08c363f8f1c37400b7307
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-02-28 14:21:56 -0700

    Updated file location

--- tests/endpoints/microservice/api/v1.0/tones/test_create_tone.py
+++ tests/endpoints/microservice/api/v1.0/tones/test_create_tone.py
@@ -5,12 +5,11 @@ from api import microservice
 
 class CreateToneTest(unittest.TestCase):
     def setUp(self):
-        dream_text = "It's too bad. When did it get cool to be so sad? We're spinnin' backwards, did we all go mad? It's too bad. When did it get cool to be so sad? We're spinnin' backwards, did we all go mad? It's too bad. When did it get cool to be so sad? We're spinnin' backwards, did we all go mad?"
+        dream_text = "It's too bad. When did it get cool to be so sad? We're spinnin' backwards, did we all go mad?"
 
         self.app = microservice()
         self.client = self.app.test_client()
         self.payload = dream_text
-
 
     def test_endpoint_microservice_api_v1_tones(self):
 
