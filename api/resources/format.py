65aa6ac1651212b6e2668717dbf7ffa3c21e083d
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-03-01 15:59:23 -0700

    Rename variables to be more clear, add comments to help future devs

--- api/resources/format.py
+++ api/resources/format.py
@@ -4,20 +4,21 @@ from collections import Counter
 class Format:
     def format_tone_results(data):
 
+        # If there is no punctuation present, return error
         if len(data["tone_analysis"]) == 1:
             return "Error: Please add punctuation to your dream journal entry"
         else:
-            new = data["tone_analysis"]["sentences_tone"]
+            sentences_tone_data = data["tone_analysis"]["sentences_tone"]
 
             def find_tone_names(data):
                 counter = 0
                 tone_names = []
                 while counter != len(data):
-                    if not data[counter]["tones"]:
+                    if not data[counter]["tones"]:  # If sentence has no tone, +1 to counter and continue
                         counter += 1  # pragma: no cover
                     else:
                         tone_in_sentence_counter = 0
-                        while tone_in_sentence_counter != len(data[counter]["tones"]):
+                        while tone_in_sentence_counter != len(data[counter]["tones"]):  # If sentence has multiple tones, add each before continuing to next sentence
                             tone_names.append(
                                 data[counter]["tones"][tone_in_sentence_counter][
                                     "tone_name"
@@ -29,12 +30,12 @@ class Format:
 
             def find_unique_tone_names(data):
                 unique_list = []
-                for x in data:
-                    if x not in unique_list:
-                        unique_list.append(f"{x}")
+                for tone in data:
+                    if tone not in unique_list:
+                        unique_list.append(f"{tone}")
                 return ", ".join(unique_list)
 
-            all_tones = find_tone_names(new)
+            all_tones = find_tone_names(sentences_tone_data)
             unique_tones = find_unique_tone_names(all_tones)
             strength_of_tone = Counter(all_tones)
 
