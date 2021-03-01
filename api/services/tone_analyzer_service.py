df5c37af18a00da445c8bf7cd59d0a4f7259596e
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-03-01 15:25:19 -0700

    Code styling

--- api/services/tone_analyzer_service.py
+++ api/services/tone_analyzer_service.py
@@ -4,25 +4,26 @@ from ibm_watson import ToneAnalyzerV3
 from ibm_watson.tone_analyzer_v3 import ToneInput
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
+
 class ToneAnalyzerService:
     @classmethod
     def get_tones(self, dream_text):
 
         # Authentication via IAM
-        authenticator = IAMAuthenticator(os.getenv('TONE_ANALYZER_API_KEY'))
+        authenticator = IAMAuthenticator(os.getenv("TONE_ANALYZER_API_KEY"))
 
-        service = ToneAnalyzerV3(
-            version='2017-09-21',
-            authenticator=authenticator)
+        service = ToneAnalyzerV3(version="2017-09-21", authenticator=authenticator)
 
-        service.set_service_url('https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/35694f05-d1b2-4c03-9773-721372d1daf0')
+        service.set_service_url(
+            "https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/35694f05-d1b2-4c03-9773-721372d1daf0"
+        )
 
         def french_or_english(dream_text):
             lang = langid.classify(dream_text)
-            if lang[0] == 'fr':
-                return 'fr'
+            if lang[0] == "fr":
+                return "fr"
             else:
-                return 'en'
+                return "en"
 
         language = french_or_english(dream_text)
 
@@ -32,7 +33,7 @@ class ToneAnalyzerService:
             tone_input=tone_input,
             content_type="application/json",
             content_language=language,
-            accept_language=language
+            accept_language=language,
         ).get_result()
 
         return tone
