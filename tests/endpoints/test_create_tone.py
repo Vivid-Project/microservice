d2ac96bd5a1d6c0429576b994f4fdff7eae660bc
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-03-01 15:26:55 -0700

    Code styling

--- tests/endpoints/test_create_tone.py
+++ tests/endpoints/test_create_tone.py
@@ -3,6 +3,7 @@ import unittest
 from copy import deepcopy
 from api import microservice
 
+
 class CreateToneTest(unittest.TestCase):
     def setUp(self):
         dream_text = "It's too bad. When did it get cool to be so sad? We're spinnin' backwards, did we all go mad? It's too bad. When did it get cool to be so sad? We're spinnin' backwards, did we all go mad? It's too bad. When did it get cool to be so sad? We're spinnin' backwards, did we all go mad?"
@@ -11,21 +12,22 @@ class CreateToneTest(unittest.TestCase):
         self.client = self.app.test_client()
         self.payload = dream_text
 
-
     def test_endpoint_microservice_api_v1_tones(self):
 
         payload = deepcopy(self.payload)
 
         response = self.client.post(
-            '/microservice/api/v1.0/tones', json=payload,
-            content_type='application/json'
+            "/microservice/api/v1.0/tones",
+            json=payload,
+            content_type="application/json",
         )
         self.assertEqual(200, response.status_code)
 
-        data = json.loads(response.data.decode('utf-8'))
+        data = json.loads(response.data.decode("utf-8"))
 
-        self.assertIsNotNone(data['tone_strength'])
-        self.assertIsNotNone(data['unique_tones'])
+        self.assertIsNotNone(data["tone_strength"])
+        self.assertIsNotNone(data["unique_tones"])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
