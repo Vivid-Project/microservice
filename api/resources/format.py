d530e874760476091415aac8773c8c6f4b42d528
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-03-01 15:23:51 -0700

    Code styling

--- api/resources/format.py
+++ api/resources/format.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+
 class Format:
     def format_tone_results(data):
 
@@ -13,11 +14,15 @@ class Format:
                 tone_names = []
                 while counter != len(data):
                     if not data[counter]["tones"]:
-                        counter += 1 # pragma: no cover
+                        counter += 1  # pragma: no cover
                     else:
                         tone_in_sentence_counter = 0
                         while tone_in_sentence_counter != len(data[counter]["tones"]):
-                            tone_names.append(data[counter]["tones"][tone_in_sentence_counter]["tone_name"])
+                            tone_names.append(
+                                data[counter]["tones"][tone_in_sentence_counter][
+                                    "tone_name"
+                                ]
+                            )
                             tone_in_sentence_counter += 1
                         counter += 1
                 return tone_names
@@ -26,14 +31,11 @@ class Format:
                 unique_list = []
                 for x in data:
                     if x not in unique_list:
-                        unique_list.append(f'{x}')
+                        unique_list.append(f"{x}")
                 return ", ".join(unique_list)
 
             all_tones = find_tone_names(new)
             unique_tones = find_unique_tone_names(all_tones)
             strength_of_tone = Counter(all_tones)
 
-            return {
-                "unique_tones": unique_tones,
-                "tone_strength": strength_of_tone
-        }
+            return {"unique_tones": unique_tones, "tone_strength": strength_of_tone}
